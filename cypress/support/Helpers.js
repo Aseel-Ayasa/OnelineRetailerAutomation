7cfeaf9d3a6bc99a293652d9736f636c46533a0f
Aseel-Ayasa <30286315+Aseel-Ayasa@users.noreply.github.com>
2021-07-03 12:28:36 +0300

    Automated test cases

---
+++ cypress/support/Helpers.js
@@ -0,0 +1,75 @@
+export const URL = {
+
+    BaseURL:'https://www.newegg.com',
+    
+    }
+    
+    
+    export const SELECTORS = {
+      searchBox:'input[title="Search Site"]',
+      ContinueShopping: '.btn[title="Continue Shopping"]',
+      CartValue: 'div.nav-complex-title > span',
+      ShoppingCart: '[title="Shopping Cart"]',
+      ShippingItem: '#shippingItemCell',
+      AddIcon: '.card-icon-plus',
+      Btn:'[type="button"]',
+      FormCell:'.form-cell',
+      ItemNthCell: '.item-cell:nth-child',
+      Save: '.modal-footer > .btn-primary',
+      FirstName: 'form > div.form-cells > div:nth-child(1) > span',
+      LastName: 'form > div.form-cells > div:nth-child(2) > span',
+      Address: 'form > div.form-cells > div:nth-child(6) > span',
+      City: 'form > div.form-cells > div:nth-child(8) > span',
+      ZIPCode: 'form > div.form-cells > div:nth-child(10) > span',
+      Phone: 'form > div.form-cells > div:nth-child(12) > span',
+      Email: 'form > div.form-cells > div:nth-child(15) > span',
+    
+      FirstNameInput: '[name="FirstName"]',
+      LastNameInput: 'input[name="LastName"]',
+      AddressInput: 'input[name="Address1"]',
+      CityInput: 'input[name="City"]',
+      ZIPCodeInput: 'input[name="ZipCode"]',
+      PhoneInput: 'input[name="Phone"]',
+      EmailInput: 'input[name="Email"]',
+
+       
+     
+    }
+    
+    export const KeyWords= {
+      SecureCheckOut: ' Secure Checkout ',
+      GuestCheckOut: 'Continue With Guest Checkout',
+      Save: 'Save',
+      ContinueToDelivery: 'Continue to delivery',
+      UseAddressAsEntered: 'Use Address as Entered',
+      AddToCart:'Add to cart '
+    
+    }
+    
+    export const ERRORMESSAGES= {
+    
+    
+      FirtNameErr: 'First name is required.',
+      LastNameErr:  'Last name is required.',
+      AddressErr: 'Address is required.',
+      CityErr:'City is required.', 
+      ZipCodeErr: 'ZIP Code is required.', 
+      PhoneErr: 'Phone is required.',
+      EmailErr: 'Email is required.'
+    
+     
+    }
+    
+    
+    
+    export const ShippingAddressData= {
+    
+     FirstName: 'Aseel',
+     LastName: 'Ayasa',
+     Address: 'Ramallah Al-Masyoun',
+     City: 'Ramallah',
+     ZipCode: '99501',
+     Phone: '0598782391',
+     Email: 'aseelayasa@gmail.com'
+    
+    }
